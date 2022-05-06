94d88aac8fc25a9e36249e7530c1b4ca04726394
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-05-06 16:59:04 +0530

    Added: Make REST Call to show Hello Mark from BridgeLabz

--- src/main/java/com/example/restapicalls/controller/RestControllerApi.java
+++ src/main/java/com/example/restapicalls/controller/RestControllerApi.java
@@ -1,19 +1,23 @@
 package com.example.restapicalls.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class RestControllerApi {
+    //curl localhost:8080/hello -w "\n"
     @RequestMapping(value = {"", "/", "home"})
     public String sayHello() {
         return "Hello From Bridgelabz..";
     }
+    //curl localhost:8080/hello/query?name=Neeta-w "\n"
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(name= "name")String name){
         return "Hello "+ name +" From Bridgelabz";
+    }
+    //curl localhost:8080/hello/param/Neeta -w"\n"
+    @GetMapping("/param/{name}")
+    public String sayHelloPara(@PathVariable String name) {
+        return "Hello " + name + " From Bridgelabz";
     }
 }
